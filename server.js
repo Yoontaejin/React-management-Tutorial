0a313bf459ec3601248792d1ae44d687ab366824
taejin <nicetaejin@rosesystems.kr>
2020-02-11 17:30:03 +0900

    client 와 server 구축 및 연동

--- server.js
+++ server.js
@@ -6,9 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
-app.get('/api/hello', (req, res) => {
-    //res.send({message : 'Hello Express !!'});
-    res.send('Hello Express !!');
+app.get('/api/customers', (req, res) => {
+    res.send([
+            {
+                'id' : 1,
+                'image' : 'https://placeimg.com/64/64/1',
+                'name': '테스터1',
+                'birthday': '20200211',
+                'gender': '남자',
+                'job': '대학생'
+            },
+            {
+                'id' : 2,
+                'image' : 'https://placeimg.com/64/64/2',
+                'name': '테스터2',
+                'birthday': '20200211',
+                'gender': '남자',
+                'job': '대학생'
+            },
+            {
+                'id' : 3,
+                'image' : 'https://placeimg.com/64/64/3',
+                'name': '테스터3',
+                'birthday': '20200211',
+                'gender': '남자',
+                'job': '대학생'
+            }
+        ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
