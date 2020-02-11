00970123e235307470bbabb4f105aa12d65ae07f
taejin <nicetaejin@rosesystems.kr>
2020-02-11 14:10:55 +0900

    고객컴포넌트 추가 생성ㅇ

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  Customer from './components/customer';
 
-class App extends React.Component {
+const customer = {
+  'name': '테스터',
+  'birthday': '20200211',
+  'gender': '남자',
+  'job': '대학생'
+}
+  class App extends React.Component {
   render() {
     return(
-        <div className="gray-background">
-          <img src={logo} lat="logo"/>
-          <h2>Let's develop management system..</h2>
-        </div>
+        <Customer
+          name={customer.name}
+          birthday={customer.birthday}
+          gender={customer.gender}
+          job={customer.job}
+        />
     );
   }
 }
